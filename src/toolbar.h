e6df59d3652d23d8daf6bf3566dac9c7e4290533
Andy Stewart <lazycat.manatee@gmail.com>
2017-03-04 08:43:38 +0800

    First commit.

---
+++ src/toolbar.h
@@ -0,0 +1,15 @@
+#include <QWidget>
+#include <QHBoxLayout>
+
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+class Toolbar : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    Toolbar(QWidget *parent = 0);
+};
+
+#endif
