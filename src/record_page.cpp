2ffd66ae27c0347c579872e89a92d6d277f2361d
Andy Stewart <lazycat.manatee@gmail.com>
2017-03-04 21:28:58 +0800

    Add wave form.

--- src/record_page.cpp
+++ src/record_page.cpp
@@ -2,13 +2,18 @@
 #include <QStandardPaths>
 #include <QDir>
 #include <QDate>
+#include <QTime>
 #include "record_page.h"
 #include "utils.h"
 
+#include <QDebug>
+
 #include <QAudioRecorder>
+#include <QAudioProbe>
 #include <QUrl>
 
 #include "dimagebutton.h"
+#include "waveform.h"
 
 DWIDGET_USE_NAMESPACE
 
@@ -17,6 +22,10 @@ RecordPage::RecordPage(QWidget *parent) : QWidget(parent)
     layout = new QVBoxLayout();
     setLayout(layout);
 
+    waveform = new Waveform();
+    
+    qDebug() << waveform->geometry();
+    
     recordTimeLabel = new QLabel("00:00");
 
     recordButton = new DImageButton(
@@ -26,10 +35,19 @@ RecordPage::RecordPage(QWidget *parent) : QWidget(parent)
         );
 
 
+    layout->addWidget(waveform);
     layout->addWidget(recordTimeLabel, 0, Qt::AlignCenter);
     layout->addWidget(recordButton, 0, Qt::AlignCenter);
-
+    
     audioRecorder = new QAudioRecorder(this);
+    
+    audioProbe = new QAudioProbe(this);
+    if (audioProbe->setSource(audioRecorder)) {
+        connect(audioProbe, SIGNAL(audioBufferProbed(QAudioBuffer)), this, SLOT(renderLevel(QAudioBuffer)));
+        
+        connect(this, SIGNAL(updateLevel(float)), waveform, SLOT(updateWave(float)));
+    }
+    
     startRecord();
 }
 
@@ -49,4 +67,14 @@ QString RecordPage::getRecordingFilepath()
     QDateTime date = QDateTime::currentDateTime();
 
     return QDir(recordDirectory).filePath(QString("%1_%2.wav").arg("deepin-voice-recorder").arg(date.toString("yyyyMMddhhmmss")));
+}
+
+void RecordPage::renderLevel(const QAudioBuffer &buffer)
+{
+    // qDebug() << volume;
+    // const float sample = qAbs(*reinterpret_cast<const float*>(buffer.constData()));
+    const quint8 *data = buffer.constData<quint8>();
+    quint8 value = *data;    
+    
+    emit updateLevel(value);
 }
