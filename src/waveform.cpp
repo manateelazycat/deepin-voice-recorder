7f208e1cd253f91ade4463237730cb4dccd1df17
Andy Stewart <lazycat.manatee@gmail.com>
2017-03-04 21:43:44 +0800

    Add linear color.

--- src/waveform.cpp
+++ src/waveform.cpp
@@ -13,7 +13,7 @@
 Waveform::Waveform(QWidget *parent) : QWidget(parent)
 {
     setFixedHeight(100);
-    
+
     lastSampleTime = QDateTime::currentDateTime();
 
     renderTimer = new QTimer();
@@ -28,25 +28,28 @@ void Waveform::paintEvent(QPaintEvent *)
 
     int volume = 0;
     for (int i = 0; i < sampleList.size(); i++) {
-        QPainterPath path;
         volume = sampleList[i] * rect().height() / 300;
-        path.addRect(QRectF(rect().x() + i * 4, rect().y() + (rect().height() - volume) / 2, 2, volume));
-        painter.fillPath(path, QColor("#ff005c"));
+        QRect sampleRect(rect().x() + i * 4, rect().y() + (rect().height() - volume) / 2, 2, volume);
+        
+        QLinearGradient gradient(sampleRect.topLeft(), sampleRect.bottomLeft());
+        gradient.setColorAt(0, QColor("#ffbd78"));
+        gradient.setColorAt(1, QColor("#ff005c"));
+        painter.fillRect(sampleRect, gradient);
     }
 }
 
 void Waveform::updateWave(float sample)
 {
     QDateTime currentTime = QDateTime::currentDateTime();
-    
+
     if (lastSampleTime.msecsTo(currentTime) > 30) {
         if (sampleList.size() > rect().width() / 4) {
             sampleList.pop_front();
         }
         sampleList << sample;
-        
+
         qDebug() << sample;
-        
+
         lastSampleTime = currentTime;
     }
 }
