3af3f131da3fb0f387c124844efe5704e31e1285
Andy Stewart <lazycat.manatee@gmail.com>
2017-03-12 12:31:03 +0800

    Update icons.

--- src/toolbar.cpp
+++ src/toolbar.cpp
@@ -9,7 +9,7 @@ Toolbar::Toolbar(QWidget *parent) : QWidget(parent)
 {
     QHBoxLayout *layout = new QHBoxLayout(this);
     
-    QPixmap iconPixmap = QPixmap(Utils::getQrcPath("logo.png"));
+    QPixmap iconPixmap = QPixmap(Utils::getQrcPath("titlebar_icon.png"));
     QLabel *iconLabel = new QLabel();
     iconLabel->setPixmap(iconPixmap);
     
