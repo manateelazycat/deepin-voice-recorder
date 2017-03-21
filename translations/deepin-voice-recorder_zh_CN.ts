729d3319a297dc774e03a304388e1b17d84679bd
Andy Stewart <lazycat.manatee@gmail.com>
2017-03-21 10:49:19 +0800

    Adjust tr string.

--- translations/deepin-voice-recorder_zh_CN.ts
+++ translations/deepin-voice-recorder_zh_CN.ts
@@ -43,12 +43,12 @@
         <translation>打开保存目录</translation>
     </message>
     <message>
-        <source>Deepin Voice Recorder is a beautiful andeasy to use voice recording application with simple design. It supports visual recording, recording playback, recording list management and other functions.</source>
-        <translation>深度录音是一款设计简洁、美观易用的录音软件。它支持可视化录音、录音回放、录音列表管理等功能。</translation>
-    </message>
-    <message>
         <source>Exit</source>
         <translation>退出</translation>
+    </message>
+    <message>
+        <source>Deepin Voice Recorder is a beautiful and easy to use voice recording application with simple design. It supports visual recording, recording playback, recording list management and other functions.</source>
+        <translation>深度录音是一款设计简洁、美观易用的录音软件。它支持可视化录音、录音回放、录音列表管理等功能。</translation>
     </message>
 </context>
 <context>
