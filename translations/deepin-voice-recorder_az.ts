5b3567ac9ef9b092b457214472c411c868eb1162
Hualet Wang <mr.asianwang@gmail.com>
2017-04-01 15:21:26 +0800

    setup tx configuration and pull translations

--- translations/deepin-voice-recorder_az.ts
+++ translations/deepin-voice-recorder_az.ts
@@ -1,73 +1,73 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="az" version="2.1">
 <context>
     <name>FileView</name>
     <message>
         <source>Rename</source>
-        <translation>重命名</translation>
+        <translation>Yenidən adlandır</translation>
     </message>
     <message>
         <source>Display in file manager</source>
-        <translation>在文件管理器中显示</translation>
+        <translation>Fayl menecerində göstər</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation>删除</translation>
+        <translation>Sil</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>Yeni qeyd</translation>
     </message>
     <message>
         <source>Open saved directory</source>
-        <translation>打开保存目录</translation>
+        <translation>Yadda saxlananlar rəhbərliyini aç</translation>
     </message>
     <message>
         <source>About</source>
-        <translation>关于</translation>
+        <translation>Haqqında</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation>帮助</translation>
+        <translation>Kömək</translation>
     </message>
     <message>
         <source>Exit</source>
-        <translation>退出</translation>
+        <translation>Çıxış</translation>
     </message>
     <message>
         <source>Deepin Voice Recorder is a beautiful and easy to use voice recording application with simple design. It supports visual recording, recording playback, recording list management and other functions.</source>
-        <translation>深度录音是一款设计简洁、美观易用的录音软件。它支持可视化录音、录音回放、录音列表管理等功能。</translation>
+        <translation>Deepin Voice Recorder səsyazmalardan istifadə etmək üçün sadə dizaynlı, gözəl və asand bir tətbiqetmədir. O vizual səsyazma, səsyazmanın təkrar oxudulması, səsyazma listinin idarə edilməsi və digər funksiyaları dəstəkləyir.</translation>
     </message>
     <message>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>Deepin Səs Yazıcı</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation>版本</translation>
+        <translation>Versiya</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>Deepin Səs Yazıcı</translation>
     </message>
 </context>
 <context>
     <name>RecordPage</name>
     <message>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>Yeni qeyd</translation>
     </message>
 </context>
 <context>
     <name>Utils</name>
     <message>
         <source>Recording</source>
-        <translation>录音</translation>
+        <translation>Qeyd olunur</translation>
     </message>
 </context>
 </TS>
--- translations/deepin-voice-recorder_cs.ts
+++ translations/deepin-voice-recorder_cs.ts
@@ -1,73 +1,73 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="cs" version="2.1">
 <context>
     <name>FileView</name>
     <message>
         <source>Rename</source>
-        <translation>重命名</translation>
+        <translation>Přejmenovat</translation>
     </message>
     <message>
         <source>Display in file manager</source>
-        <translation>在文件管理器中显示</translation>
+        <translation>Zobrazit ve správci souborů</translation>
     </message>
     <message>
         <source>Delete</source>
-        <translation>删除</translation>
+        <translation>Smazat</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>Nová nahrávka</translation>
     </message>
     <message>
         <source>Open saved directory</source>
-        <translation>打开保存目录</translation>
+        <translation>Otevřít uložený adresář</translation>
     </message>
     <message>
         <source>About</source>
-        <translation>关于</translation>
+        <translation>O programu</translation>
     </message>
     <message>
         <source>Help</source>
-        <translation>帮助</translation>
+        <translation>Nápověda</translation>
     </message>
     <message>
         <source>Exit</source>
-        <translation>退出</translation>
+        <translation>Ukončit</translation>
     </message>
     <message>
         <source>Deepin Voice Recorder is a beautiful and easy to use voice recording application with simple design. It supports visual recording, recording playback, recording list management and other functions.</source>
-        <translation>深度录音是一款设计简洁、美观易用的录音软件。它支持可视化录音、录音回放、录音列表管理等功能。</translation>
+        <translation>Nahrávač hlasu je překrásný, jednoduchý a snadno použitelný program na nahrávání hlasu. Podporuje zrakové nahrávání, přehrávání nahrávky, správu seznamu záznamů a má i jiné funkce.</translation>
     </message>
     <message>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>Nahrávač hlasu</translation>
     </message>
     <message>
         <source>Version</source>
-        <translation>版本</translation>
+        <translation>Verze</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>Nahrávač hlasu</translation>
     </message>
 </context>
 <context>
     <name>RecordPage</name>
     <message>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>Nová nahrávka</translation>
     </message>
 </context>
 <context>
     <name>Utils</name>
     <message>
         <source>Recording</source>
-        <translation>录音</translation>
+        <translation>Nahrávání</translation>
     </message>
 </context>
 </TS>
